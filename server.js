9f3f8d11be0f6a8ee25902f8da6f85821fd52b27
Luka Kidrič <luka.kidric@kolektor.com>
2021-08-29 20:33:42 +0200

    added logic and docker files

--- server.js
+++ server.js
@@ -3,20 +3,26 @@
 const express = require('express');
 
 // Constants
-const PORT = 8020;
-const HOST = 'localhost';
+
 
 const textArray = ["Hey, welcome to the GET side of things :)","Oh no, not you again","Are you lost perhaps ?","Someone looks dissapointed","Well how do you like it here ? :)"];
 
+function getTime(){
+    const date = new Date();
+    date.setHours(date.getHours() + 2);
+    const isoDate = date.toISOString();
+    return isoDate;
+}
 // App
 const app = express();
-app.get("/data", (req, res, next) => {
+app.get("/:accountId/data", (req, res, next) => {
     res.json({
-        accountId: "accountId",
-        timestamp: new Date().toISOString(),
+        accountId: req.params.accountId,
+        timestamp: getTime(),
         data: textArray[Math.floor(Math.random() * textArray.length)], 
     });
    });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(80, function(){
+    console.log("App running");
+});
